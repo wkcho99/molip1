22054d8b8a3ac62adb260cfab88dea117810c072
wkcho99 <wkcho@postech.ac.kr>
2022-01-04 02:24:30 +0900

    pedometer complete

--- app/src/main/java/com/example/week1/ui/main/Tab3Activity.java
+++ app/src/main/java/com/example/week1/ui/main/Tab3Activity.java
@@ -1,6 +1,7 @@
 package com.example.week1.ui.main;
 import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
+import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
@@ -14,126 +15,53 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.media.Image;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.week1.PreferenceManager;
 import com.example.week1.R;
 
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
 public class Tab3Activity extends Fragment implements SensorEventListener {
-//    Button mReset;
-//    TextView mwalknum;
-//    //현재 걸음 수
-//    private int mSteps = 0;
-//    //리스너가 등록되고 난 후의 step count
-//    private int mCounterSteps = 0;
-//
-//
-//    //센서 연결을 위한 변수
-//    private SensorManager sensorManager;
-//    //private Sensor accelerormeterSensor;
-//    private Sensor stepCountSensor;
-//
-//
-//    private View view;
-//
-//
-//    public Tab3Activity() {
-//        // Required empty public constructor
-//    }
-//
-//
-//    @Override
-//    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-//                             Bundle savedInstanceState) {
-//        view = inflater.inflate(R.layout.fragment_third, container, false);
-//
-//        //센서 연결[걸음수 센서를 이용한 흔듬 감지]
-//        sensorManager = (SensorManager)getActivity().getSystemService(Context.SENSOR_SERVICE);
-//        //accelerormeterSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-//        stepCountSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
-//
-//        if(stepCountSensor == null){
-//            // Toast.makeText(this,"No Step Detect Sensor",Toast.LENGTH_SHORT).show();
-//        }
-//
-//        mReset = view.findViewById(R.id.button6);
-//        mwalknum = view.findViewById(R.id.textView2);
-//
-//        //초기화 버튼 : 다시 숫자를 0으로 만들어준다.
-//        mReset.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                mSteps = 0;
-//                mCounterSteps = 0;
-//                mwalknum.setText(Integer.toString(mSteps));
-//
-//            }
-//        });
-//
-//
-//        return view;
-//    }
-//
-//    public void onStart() {
-//        super.onStart();
-//        if(stepCountSensor !=null){
-//            //센서의 속도 설정
-//            sensorManager.registerListener(this,stepCountSensor,SensorManager.SENSOR_DELAY_NORMAL);
-//        }
-//    }
-//    public void onStop(){
-//        super.onStop();
-//        if(sensorManager!=null){
-//            sensorManager.unregisterListener(this);
-//        }
-//    }
-//
-//    @Override
-//    public void onSensorChanged(SensorEvent event) {
-//        mCounterSteps = PreferenceManager.getInt(getActivity(),"rebuild");
-//        if(event.sensor.getType() == Sensor.TYPE_STEP_COUNTER){
-//            //stepcountsenersor는 앱이 꺼지더라도 초기화 되지않는다. 그러므로 우리는 초기값을 가지고 있어야한다.
-//            if (mCounterSteps < 1) {
-//                // initial value
-//                mCounterSteps = (int) event.values[0]-mSteps;
-//                PreferenceManager.setInt(getActivity(),"rebuild",mCounterSteps);
-//                Log.i("mCounterSteps:",Integer.toString(mCounterSteps));
-//                Log.i("mCounterSteps:",Integer.toString(mCounterSteps));
-//                Log.i("mSteps:",Integer.toString(mSteps));
-//            }
-//            //리셋 안된 값 + 현재값 - 리셋 안된 값
-//            Log.i("mCounterSteps:",Integer.toString(mCounterSteps));
-//            Log.i("event.values[0]:",Integer.toString((int) event.values[0]));
-//            mSteps = (int) event.values[0] - mCounterSteps;
-//            mwalknum.setText(Integer.toString(mSteps));
-//            Log.i("log: ", "New step detected by STEP_COUNTER sensor. Total step count: " + mSteps );
-//        }
-//
-//    }
-//
-//    public void onAccuracyChanged(Sensor sensor, int accuracy) {
-//
-//    }
-
-
-
     SensorManager sensorManager;
     Sensor stepCountSensor;
     TextView stepCountView;
+    TextView timeView;
+    TextView passView;
+    ImageView imageView;
     Button resetButton;
-    // 현재 걸음 수
+    Thread tr;
     int currentSteps = 0;
-    //
     private Context context;
     private ContentResolver contentResolver;
+    long mNow;
+    long mPass;
+    Date mDate;
+
+
+    SimpleDateFormat mFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+    String startTime = "Press RESET button";
+    String passTime = "";
     public static Tab3Activity newInstance() {
         Tab3Activity fragment = new Tab3Activity();
         Bundle bundle = new Bundle();
@@ -151,13 +79,11 @@ public class Tab3Activity extends Fragment implements SensorEventListener {
 
             requestPermissions(new String[]{Manifest.permission.ACTIVITY_RECOGNITION}, 0);
         }
-        // 걸음 센서 연결
-        // * 옵션
-        // - TYPE_STEP_DETECTOR:  리턴 값이 무조건 1, 앱이 종료되면 다시 0부터 시작
-        // - TYPE_STEP_COUNTER : 앱 종료와 관계없이 계속 기존의 값을 가지고 있다가 1씩 증가한 값을 리턴
-        //
-
         currentSteps = PreferenceManager.getInt(getActivity(),"rebuild");
+        startTime = PreferenceManager.getString(getActivity(),"starttime");
+        passTime = PreferenceManager.getString(getActivity(),"passtime");
+        mNow = PreferenceManager.getLong(getActivity(),"mnow");
+        Log.i("oncreate:",(startTime));
         Log.i("oncreate:",Integer.toString(currentSteps));
         // 리셋 버튼 추가 - 리셋 기능
 
@@ -174,16 +100,66 @@ public class Tab3Activity extends Fragment implements SensorEventListener {
         if (stepCountSensor == null) {
             Toast.makeText(getActivity(), "No Step Sensor", Toast.LENGTH_SHORT).show();
         }
+
         View root = inflater.inflate(R.layout.fragment_third, container, false);
         stepCountView = root.findViewById(R.id.textView2);
         resetButton = root.findViewById(R.id.button6);
+        timeView = root.findViewById(R.id.textView);
+        passView = root.findViewById(R.id.textView3);
+        imageView = root.findViewById(R.id.imageView);
         stepCountView.setText(String.valueOf(currentSteps));
+        timeView.setText("Start: "+startTime);
+        final Animation runningAnim = AnimationUtils.loadAnimation(context,R.anim.running);
+        final Animation growingAnim = AnimationUtils.loadAnimation(context,R.anim.growing);
+        if(currentSteps < 10) {
+            imageView.setImageResource(R.drawable.fish);
+        }
+                if(currentSteps >= 10) {
+                    imageView.setImageResource(R.drawable.ingfish);
+                }
+                if(currentSteps >= 20) {
+                    imageView.setImageResource(R.drawable.gyara);
+                }
+        Log.i("startTime:", startTime);
+        tr = new Thread(new Runnable()
+        {
+
+            @Override
+            public void run()
+            {
+                while (!Thread.interrupted())
+                    try
+                    {
+                        Thread.sleep(1000);
+                        getActivity().runOnUiThread(new Runnable() // start actions in UI thread
+                        {
+
+                            @Override
+                            public void run()
+                            {
+                                passView.setText("Elapsed Time: "+passTime());
+                                PreferenceManager.setString(context,"passtime",passTime);
+                            }
+                        });
+                    }
+                    catch (InterruptedException e)
+                    {
+                        // ooops
+                    }
+
+            }
+        });
+        tr.start();
         resetButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // 현재 걸음수 초기화
                 currentSteps = 0;
                 stepCountView.setText(String.valueOf(currentSteps));
+                timeView.setText("Start: "+ getTime());
+                PreferenceManager.setString(context,"starttime",getTime());
+                imageView.setImageResource(R.drawable.fish);
+                Log.i("startTime:", startTime);
                 Log.i("onclick:",Integer.toString(currentSteps));
             }
         });
@@ -193,13 +169,6 @@ public class Tab3Activity extends Fragment implements SensorEventListener {
     public void onStart() {
         super.onStart();
         if(stepCountSensor !=null) {
-            // 센서 속도 설정
-            // * 옵션
-            // - SENSOR_DELAY_NORMAL: 20,000 초 딜레이
-            // - SENSOR_DELAY_UI: 6,000 초 딜레이
-            // - SENSOR_DELAY_GAME: 20,000 초 딜레이
-            // - SENSOR_DELAY_FASTEST: 딜레이 없음
-            //
             sensorManager.registerListener(this,stepCountSensor,SensorManager.SENSOR_DELAY_FASTEST);
         }
     }
@@ -209,22 +178,155 @@ public class Tab3Activity extends Fragment implements SensorEventListener {
     @Override
     public void onSensorChanged(SensorEvent event) {
         // 걸음 센서 이벤트 발생시
+        final Animation runningAnim = AnimationUtils.loadAnimation(context,R.anim.running);
+        final Animation growingAnim = AnimationUtils.loadAnimation(context,R.anim.growing);
         if(event.sensor.getType() == Sensor.TYPE_STEP_DETECTOR){
             if(event.values[0]==1.0f){
-                // 센서 이벤트가 발생할때 마다 걸음수 증가
+                imageView.startAnimation(runningAnim);
                 currentSteps++;
                 stepCountView.setText(String.valueOf(currentSteps));
+                if(currentSteps == 10) {
+                    imageView.setImageResource(R.drawable.ingfish);
+                    imageView.startAnimation(growingAnim);
+                }
+                if(currentSteps == 20) {
+                    imageView.setImageResource(R.drawable.gyara);
+                    imageView.startAnimation(growingAnim);
+                }
                 PreferenceManager.setInt(context,"rebuild",currentSteps);
             }
 
 
         }
-
     }
-
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
 
     }
+    private String getTime(){
+        mNow = System.currentTimeMillis();
+        PreferenceManager.setLong(getActivity(),"mnow",mNow);
+        mDate = new Date(mNow);
+        return mFormat.format(mDate);
+    }
+    private String passTime(){
+        long temp = System.currentTimeMillis();
+        Log.i("now:",Long.toString(mNow));
+        Log.i("temp:",Long.toString(temp));
+
+        mPass = temp -mNow;
+        long hour = mPass/(60*60*1000);
+        long min = (mPass/(60*1000))%60;
+        long sec = (mPass/(1000))%60;
+        return hour+":"+min+":"+sec;
+    }
+    @Override
+
+    public void onDestroy( ) {
+
+        super.onDestroy( );
+
+        tr.interrupt( );
+
+    }
+
+
+//    @Override
+//    public void onMapReady(GoogleMap googleMap) {
+//        mMap = googleMap;
+//
+//        setDefaultLocation(); // GPS를 찾지 못하는 장소에 있을 경우 지도의 초기 위치가 필요함.
+//
+//        getLocationPermission();
+//
+//        updateLocationUI();
+//
+//        getDeviceLocation();
+//    }
+//
+//    private void getLocationPermission() {
+//        if (ContextCompat.checkSelfPermission(context,
+//                android.Manifest.permission.ACCESS_FINE_LOCATION)
+//                == PackageManager.PERMISSION_GRANTED) {
+//            mLocationPermissionGranted = true;
+//        } else {
+//            ActivityCompat.requestPermissions(getActivity(),
+//                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+//                    PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+//        }
+//    }
+//    private void getDeviceLocation() {
+//        try {
+//            if (mLocationPermissionGranted) {
+//                mFusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, Looper.myLooper());
+//            }
+//        } catch (SecurityException e)  {
+//            Log.e("Exception: %s", e.getMessage());
+//        }
+//    }
+//
+//    private void setDefaultLocation() {
+//        if (currentMarker != null) currentMarker.remove();
+//
+//        MarkerOptions markerOptions = new MarkerOptions();
+//        markerOptions.position(mDefaultLocation);
+//        markerOptions.title("위치정보 가져올 수 없음");
+//        markerOptions.snippet("위치 퍼미션과 GPS 활성 여부 확인하세요");
+//        markerOptions.draggable(true);
+//        markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED));
+//        currentMarker = mMap.addMarker(markerOptions);
+//
+//        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(mDefaultLocation, 15);
+//        mMap.moveCamera(cameraUpdate);
+//    }
+//
+//    String getCurrentAddress(LatLng latlng) {
+//        // 위치 정보와 지역으로부터 주소 문자열을 구한다.
+//        List<Address> addressList = null ;
+//        Geocoder geocoder = new Geocoder( context, Locale.getDefault());
+//
+//        // 지오코더를 이용하여 주소 리스트를 구한다.
+//        try {
+//            addressList = geocoder.getFromLocation(latlng.latitude,latlng.longitude,1);
+//        } catch (IOException e) {
+//            Toast. makeText( context, "위치로부터 주소를 인식할 수 없습니다. 네트워크가 연결되어 있는지 확인해 주세요.", Toast.LENGTH_SHORT ).show();
+//            e.printStackTrace();
+//            return "주소 인식 불가" ;
+//        }
+//
+//        if (addressList.size() < 1) { // 주소 리스트가 비어있는지 비어 있으면
+//            return "해당 위치에 주소 없음" ;
+//        }
+//
+//        // 주소를 담는 문자열을 생성하고 리턴
+//        Address address = addressList.get(0);
+//        StringBuilder addressStringBuilder = new StringBuilder();
+//        for (int i = 0; i <= address.getMaxAddressLineIndex(); i++) {
+//            addressStringBuilder.append(address.getAddressLine(i));
+//            if (i < address.getMaxAddressLineIndex())
+//                addressStringBuilder.append("\n");
+//        }
+//
+//        return addressStringBuilder.toString();
+//    }
+//    private void updateLocationUI() {
+//        if (mMap == null) {
+//            return;
+//        }
+//        try {
+//            if (mLocationPermissionGranted) {
+//                mMap.setMyLocationEnabled(true);
+//                mMap.getUiSettings().setMyLocationButtonEnabled(true);
+//            } else {
+//                mMap.setMyLocationEnabled(false);
+//                mMap.getUiSettings().setMyLocationButtonEnabled(false);
+//                mCurrentLocatiion = null;
+//                getLocationPermission();
+//            }
+//        } catch (SecurityException e)  {
+//            Log.e("Exception: %s", e.getMessage());
+//        }
+//    }
+
 }
